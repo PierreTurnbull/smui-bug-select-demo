5daa04b42b54c0e788575943d0c0b66bc14f5f2f
pierre.turnbull-ext <pierre.turnbull-ext@vinci-construction.com>
2024-04-22 08:54:35 +0200

    a

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Select from "@smui/select";
+    import { Modal } from "flowbite-svelte"
+
+    let flowbiteOpen = true
+</script>
+<Modal
+    open={flowbiteOpen}
+>
+    flowbite modal
+    <Select></Select>
+    <button on:click={() => flowbiteOpen = false}>close dialog</button>
+</Modal>
